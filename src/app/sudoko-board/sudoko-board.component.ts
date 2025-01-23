b67f27f44879b8b8c82c040b5dbf2c8617e44799
SaadHassanRazi <saadhassanrazi@gmail.com>
2025-01-23 15:39:44 +0500

    fix commit

--- src/app/sudoko-board/sudoko-board.component.ts
+++ src/app/sudoko-board/sudoko-board.component.ts
@@ -149,6 +149,15 @@ export class SudokoBoardComponent implements OnInit {
       //   this.board[rowIndex][cellIndex] = 0;
       // }
     }
+    if (isNaN(num) || num < 1 || num > 9) {
+      this.cellStatus[rowIndex][cellIndex] = 'invalid'; // Mark invalid
+      input.value = ''; // Clear invalid input
+    } else {
+      this.cellStatus[rowIndex][cellIndex] = ''; // Clear invalid flag
+      this.board[rowIndex][cellIndex] = num;
+
+      
+    }
 
     // if (!isNaN(num) && num >= 1 && num <= 9) {
     //   this.board[rowIndex][cellIndex] = num;
@@ -181,30 +190,6 @@ export class SudokoBoardComponent implements OnInit {
 
   resetGameHandler(): void {
     this.board = [];
-    this.generateSudokoPuzzle(  );
+    this.generateSudokoPuzzle();
   }
 }
-
-// solution for first puzzle
-// [ 5, 3, 4, 6, 7, 8, 9, 1, 2 ]
-// [ 6, 7, 2, 1, 9, 5, 3, 4, 8 ]
-// [ 1, 9, 8, 3, 4, 2, 5, 6, 7 ]
-// [ 8, 5, 9, 7, 6, 1, 4, 2, 3 ]
-// [ 4, 2, 6, 8, 5, 3, 7, 9, 1 ]
-// [ 7, 1, 3, 9, 2, 4, 8, 5, 6 ]
-// [ 9, 6, 1, 5, 3, 7, 2, 8, 4 ]
-// [ 2, 8, 7, 4, 1, 9, 6, 3, 5 ]
-// [ 3, 4, 5, 2, 8, 6, 1, 7, 9 ]
-
-// solution for second puzzle
-// const solution = [
-//   [4, 8, 3, 9, 2, 1, 6, 5, 7],
-//   [9, 6, 7, 3, 4, 5, 8, 2, 1],
-//   [2, 5, 1, 8, 7, 6, 4, 9, 3],
-//   [5, 4, 8, 1, 3, 2, 9, 7, 6],
-//   [7, 2, 9, 5, 6, 4, 1, 3, 8],
-//   [1, 3, 6, 7, 9, 8, 2, 4, 5],
-//   [3, 7, 2, 6, 8, 9, 5, 1, 4],
-//   [8, 1, 4, 2, 5, 3, 7, 6, 9],
-//   [6, 9, 5, 4, 1, 7, 3, 8, 2],
-// ];
